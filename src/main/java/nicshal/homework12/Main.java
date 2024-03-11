2f39b45bfdcf9eb07a65c9adcc1aa9a38c366836
Nikolay Shalashov <nicshal@mail.ru>
2024-03-11 19:56:11 +0300

    homework12: Изучить принцип работы с потоками ввода/вывода

--- src/main/java/nicshal/homework12/Main.java
+++ src/main/java/nicshal/homework12/Main.java
@@ -11,6 +11,32 @@ public class Main {
     public static final int NUMBER_ATTEMPTS = 3;
 
     public static void main(String[] args) {
+        List<String> fileList = generateFileList();
+        if (fileList.isEmpty()) {
+            System.out.println("В корневом каталоге не найдено текстовых файлов");
+        } else {
+            printFileList(fileList);
+            Scanner scanner = new Scanner(System.in);
+            int count = 0;
+            while (count < NUMBER_ATTEMPTS) {
+                String inputFileName = getFileName(scanner);
+                if (fileList.contains(inputFileName)) {
+                    try (InputStreamReader in = new InputStreamReader(new FileInputStream(inputFileName));
+                         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(inputFileName, true))) {
+                        printFileContents(in);
+                        writeCommentToFileContents(out, getComment(scanner), inputFileName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                } else {
+                    System.out.println("Файл " + inputFileName + " не найден в корневом каталоге. Осталось " + (NUMBER_ATTEMPTS - ++count) + " попыток");
+                }
+            }
+        }
+    }
+
+    public static List<String> generateFileList() {
         List<String> fileList = new ArrayList<>();
         File file = new File(".");
         for (File item : file.listFiles()) {
@@ -18,52 +44,46 @@ public class Main {
                 fileList.add(item.getName());
             }
         }
+        return fileList;
+    }
 
-        if (fileList.isEmpty()) {
-            System.out.println("В корневом каталоге не найдено текстовых файлов");
-        } else {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Для просмотра и редактирования доступны следующие текстовые файлы:");
-            for (String fileName : fileList) {
-                System.out.println(fileName);
-            }
-
-            int count = 0;
-            String inputFileName;
-            while (count < NUMBER_ATTEMPTS) {
-                System.out.println("Введите имя файла, с которым Вы будете работать:");
-                inputFileName = scanner.nextLine();
-                if (fileList.contains(inputFileName)) {
-                    try (InputStreamReader in = new InputStreamReader(new FileInputStream(inputFileName));
-                         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(inputFileName, true))) {
-                        int n = in.read();
-                        while (n != -1) {
-                            System.out.print((char) n);
-                            n = in.read();
-                        }
-
-                        System.out.println("Напишите свой комментарий к прочитанному:");
-                        String comment = scanner.nextLine();
-                        if (comment.trim().isEmpty()) {
-                            System.out.println("Вы не оставили комментарий");
-                        } else {
-                            comment = "\n---\n" + comment;
-                            byte[] buffer = comment.getBytes(StandardCharsets.UTF_8);
-                            for (byte b : buffer) {
-                                out.write(b);
-                            }
-                            System.out.println("Ваш комментарий записан в файл " + inputFileName);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                } else {
-                    count += 1;
-                    System.out.println("Файл " + inputFileName + " не найден в корневом каталоге. Осталось " +
-                            (NUMBER_ATTEMPTS - count) + " попыток");
-                }
-            }
+    public static void printFileList(List<String> fileList) {
+        System.out.println("Для просмотра и редактирования доступны следующие текстовые файлы:");
+        for (String fileName : fileList) {
+            System.out.println(fileName);
         }
     }
+
+    public static void printFileContents(InputStreamReader in) throws IOException {
+        int n = in.read();
+        while (n != -1) {
+            System.out.print((char) n);
+            n = in.read();
+        }
+        System.out.println("---");
+    }
+
+    public static void writeCommentToFileContents(BufferedOutputStream out, String comment, String fileName) throws IOException {
+        if (comment.trim().isEmpty()) {
+            System.out.println("Вы не оставили комментарий");
+        } else {
+            comment = "\n---\n" + comment;
+            byte[] buffer = comment.getBytes(StandardCharsets.UTF_8);
+            for (byte b : buffer) {
+                out.write(b);
+            }
+            System.out.println("Ваш комментарий записан в файл " + fileName);
+        }
+    }
+
+    public static String getFileName(Scanner scanner) {
+        System.out.println("Введите имя файла, с которым Вы будете работать:");
+        return scanner.nextLine();
+    }
+
+    public static String getComment(Scanner scanner) {
+        System.out.println("Напишите свой комментарий к прочитанному:");
+        return scanner.nextLine();
+    }
+
 }
