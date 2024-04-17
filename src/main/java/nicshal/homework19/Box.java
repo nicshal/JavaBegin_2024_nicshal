61b65afbed47a68b09e2e5efa3137d3d1f08a409
Nikolay Shalashov <nicshal@mail.ru>
2024-04-17 21:41:09 +0300

    homework19 Работа с обобщениями

--- src/main/java/nicshal/homework19/Box.java
+++ src/main/java/nicshal/homework19/Box.java
@@ -7,10 +7,11 @@ import static nicshal.homework19.ApplicationConstants.*;
 
 public class Box<T extends Fruit> {
 
-    private List<T> fruitList = new ArrayList<>();
+    private List<T> fruitList;
     private double capacity;
 
     public Box(int capacity) {
+        fruitList = new ArrayList<>();
         this.capacity = capacity;
     }
 
@@ -18,16 +19,8 @@ public class Box<T extends Fruit> {
         return fruitList;
     }
 
-    public void setFruitList(List<T> fruitList) {
-        this.fruitList = fruitList;
-    }
-
     public double getCapacity() {
         return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
     }
 
     public void add(T fruit) {
@@ -54,13 +47,16 @@ public class Box<T extends Fruit> {
     }
 
     public void move(Box<? super T> box) {
+        if (box == null || box == this) {
+            throw new IllegalArgumentException("Метод вызван с некорректным аргементом (null либо ссылка на себя");
+        }
         if (box.getResidualCapacity() - getWeight() < -PRECISION) {
             throw new BoxDataException(String.format(BOX_MOVE_ERROR, box.getResidualCapacity(), getWeight()));
         }
         for (T fruit : fruitList) {
             box.add(fruit);
         }
-        setFruitList(new ArrayList<>());
+        fruitList = new ArrayList<>();
     }
 
     @Override
